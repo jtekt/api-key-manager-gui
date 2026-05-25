3704ebe28ed70e363be16e1e673a48322d97801e
Maxime Moreillon <m.moreillon@gmail.com>
2026-05-25 09:00:08 +0900

    cicd

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ROOT_DIR=/app
+
+# Replace env vars in files served by NGINX
+echo "Replacing environment variables"
+for file in $ROOT_DIR/assets/*.js $ROOT_DIR/index.html;
+do
+  echo "Processing $file ...";
+
+
+  sed -i 's|VITE_API_BASE_URL_PLACEHOLDER|'${VITE_API_BASE_URL}'|g' $file
+  sed -i 's|VITE_OIDC_AUTHORITY_PLACEHOLDER|'${VITE_OIDC_AUTHORITY}'|g' $file
+  sed -i 's|VITE_OIDC_CLIENT_ID_PLACEHOLDER|'${VITE_OIDC_CLIENT_ID}'|g' $file
+
+
+done
+
+echo "Starting Nginx"
+nginx -g 'daemon off;'
